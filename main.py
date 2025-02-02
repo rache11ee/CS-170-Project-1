2c4a8b6ec61b5361674eded206ad5eaf88dc27b6
rache11ee <1leerachel1@gmail.com>
2025-02-02 01:39:08 -0800

    Made the a* misplaced heuristic algorithm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from uniform_cost_search import uniform_cost_search
 from puzzle import total_path
+from misplaced_heuristic import a_star_misplaced_heuristic
 
 def print_state(state): 
     for i in range(0,9,3):
@@ -8,7 +9,7 @@ def print_state(state):
 print("write 1 for default puzzle or 2 to input your own")
 choice = int(input())
 if choice == 1:
-    initial_state = [1,2,3,4,5,6,7,0,8]
+    initial_state = [7,2,4,5,0,6,8,3,1]
     
 else:
     print("enter your own puzzle and use a zero for the blank!")
@@ -17,7 +18,14 @@ else:
         row = input(f"type in 3 numbers per row with spaces in between and press enter after each row: ").split()
         initial_state.extend(map(int,row)) #organizes the row into the initial state
 
-solution_node = uniform_cost_search(initial_state)
+print("write 1 to use a* misplaced heuristic, 2 to use a* manhattan heuristic, or 3 to use uniform cost search")
+algorithm = int(input())
+if algorithm == 1:
+    solution_node = a_star_misplaced_heuristic(initial_state)
+elif algorithm == 3:
+    solution_node = uniform_cost_search(initial_state)
+    
+
 if solution_node:
     path = total_path(solution_node)
     for state in path: #will print all of the states that it takes to get to the goal state
@@ -25,3 +33,5 @@ if solution_node:
         print("\n")
 else:
     print("No solution found!")
+
+
---
+++ misplaced_heuristic.py
@@ -0,0 +1,32 @@
+import heapq
+from node import Node
+from puzzle import get_children
+
+goal_state = [1,2,3,4,5,6,7,8,0]
+
+def a_star_misplaced_heuristic(initial_state):
+    initial_node = Node(initial_state)
+
+    node_collection = []
+    heapq.heappush(node_collection,initial_node) #initial node is being pushed into the node collection
+
+    heap_history = set()
+    
+    while node_collection:
+        current_node = heapq.heappop(node_collection) #heap will pop off the node with the smallest cost
+        if current_node.state == goal_state:
+            return current_node
+        heap_history.add(tuple(current_node.state))
+        
+        for child in get_children(current_node):
+            if tuple(child.state) not in heap_history: #if the child's path has not been explored, add it to the node collection
+                child.heuristic = misplaced_heuristic(child.state, goal_state)
+                child.cost += child.heuristic
+                heapq.heappush(node_collection,child)
+
+    return None
+
+def misplaced_heuristic(state, goal_state):
+    num_misplaced_tiles = sum(1 for state_tile, goal_tile in zip(state, goal_state) if state_tile != goal_tile and state_tile != 0) #add one for every tile that is not in the correct spot
+    return num_misplaced_tiles
+
